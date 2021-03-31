5bb869a29b78388bee6e9c9502ff07dcb2664968
Leochitralwar <57825419+ChitralwarManik@users.noreply.github.com>
2021-03-31 14:24:38 +0530

    updated new files

---
+++ main.py
@@ -0,0 +1,39 @@
+# Importing essential libraries
+import numpy as np
+import pandas as pd
+import pickle
+
+# Loading the dataset
+df = pd.read_csv('diabetes.csv')
+
+# Renaming DiabetesPedigreeFunction as DPF
+df = df.rename(columns={'DiabetesPedigreeFunction':'DPF'})
+
+# Replacing the 0 values from ['Glucose','BloodPressure','SkinThickness','Insulin','BMI'] by NaN
+df_copy = df.copy(deep=True)
+df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
+
+# Replacing NaN value by mean, median depending upon distribution
+df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
+df_copy['BloodPressure'].fillna(df_copy['BloodPressure'].mean(), inplace=True)
+df_copy['SkinThickness'].fillna(df_copy['SkinThickness'].median(), inplace=True)
+df_copy['Insulin'].fillna(df_copy['Insulin'].median(), inplace=True)
+df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
+
+# Model Building
+from sklearn.model_selection import train_test_split
+X = df.drop(columns='Outcome')
+y = df['Outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# Creating Random Forest Model
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=1/3,random_state=42, stratify=y)
+
+# import randon forest classifier from sklearn
+from sklearn.ensemble import RandomForestClassifier
+random_forest_model = RandomForestClassifier(random_state=10)
+random_forest_model.fit(X_train, y_train.ravel())
+# Creating a pickle file for the classifier
+filename = 'diabetes-prediction-rfc-model.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
